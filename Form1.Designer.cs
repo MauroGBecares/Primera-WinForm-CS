8d21768180cb3078e6e423b25f2580d1e8b77867
MauroGBecares <maurobecares26@gmail.com>
2023-01-28 13:33:47 -0300

    1.0

---
+++ Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿using System;
+
+namespace Practica5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblEdad = new System.Windows.Forms.Label();
+            this.lblDireccion = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tsslTiempo = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblApellido.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lblApellido.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblApellido.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblApellido.Location = new System.Drawing.Point(17, 48);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(84, 23);
+            this.lblApellido.TabIndex = 0;
+            this.lblApellido.Text = "APELLIDO";
+            this.lblApellido.MouseLeave += new System.EventHandler(this.lblApellido_MouseLeave);
+            this.lblApellido.MouseMove += new System.Windows.Forms.MouseEventHandler(this.lblApellido_MouseMove);
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblNombre.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lblNombre.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblNombre.Location = new System.Drawing.Point(17, 89);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(79, 23);
+            this.lblNombre.TabIndex = 1;
+            this.lblNombre.Text = "NOMBRE";
+            this.lblNombre.MouseLeave += new System.EventHandler(this.lblNombre_MouseLeave);
+            this.lblNombre.MouseMove += new System.Windows.Forms.MouseEventHandler(this.lblNombre_MouseMove);
+            // 
+            // lblEdad
+            // 
+            this.lblEdad.AutoSize = true;
+            this.lblEdad.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblEdad.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lblEdad.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEdad.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblEdad.Location = new System.Drawing.Point(17, 132);
+            this.lblEdad.Name = "lblEdad";
+            this.lblEdad.Size = new System.Drawing.Size(53, 23);
+            this.lblEdad.TabIndex = 2;
+            this.lblEdad.Text = "EDAD";
+            this.lblEdad.MouseLeave += new System.EventHandler(this.lblEdad_MouseLeave);
+            this.lblEdad.MouseMove += new System.Windows.Forms.MouseEventHandler(this.lblEdad_MouseMove);
+            // 
+            // lblDireccion
+            // 
+            this.lblDireccion.AutoSize = true;
+            this.lblDireccion.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblDireccion.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lblDireccion.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDireccion.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblDireccion.Location = new System.Drawing.Point(17, 173);
+            this.lblDireccion.Name = "lblDireccion";
+            this.lblDireccion.Size = new System.Drawing.Size(95, 23);
+            this.lblDireccion.TabIndex = 3;
+            this.lblDireccion.Text = "DIRECCIÓN";
+            this.lblDireccion.MouseLeave += new System.EventHandler(this.lblDireccion_MouseLeave);
+            this.lblDireccion.MouseMove += new System.Windows.Forms.MouseEventHandler(this.lblDireccion_MouseMove);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblResultado.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lblResultado.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(237, 228);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(137, 23);
+            this.lblResultado.TabIndex = 4;
+            this.lblResultado.Text = "SUS DATOS SON:";
+            this.lblResultado.MouseLeave += new System.EventHandler(this.lblResultado_MouseLeave);
+            this.lblResultado.MouseMove += new System.Windows.Forms.MouseEventHandler(this.lblResultado_MouseMove);
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtApellido.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtApellido.Location = new System.Drawing.Point(128, 48);
+            this.txtApellido.MaxLength = 20;
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(463, 20);
+            this.txtApellido.TabIndex = 0;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtNombre.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtNombre.Location = new System.Drawing.Point(128, 92);
+            this.txtNombre.MaxLength = 20;
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(463, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtEdad.Location = new System.Drawing.Point(128, 132);
+            this.txtEdad.MaxLength = 3;
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(40, 20);
+            this.txtEdad.TabIndex = 2;
+            this.txtEdad.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtEdad_KeyPress);
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtDireccion.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtDireccion.Location = new System.Drawing.Point(128, 173);
+            this.txtDireccion.MaxLength = 20;
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(463, 20);
+            this.txtDireccion.TabIndex = 3;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultado.Location = new System.Drawing.Point(21, 254);
+            this.txtResultado.Multiline = true;
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.ReadOnly = true;
+            this.txtResultado.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResultado.Size = new System.Drawing.Size(570, 210);
+            this.txtResultado.TabIndex = 5;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnAceptar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAceptar.Location = new System.Drawing.Point(21, 470);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(126, 28);
+            this.btnAceptar.TabIndex = 4;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = false;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnCancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCancelar.Location = new System.Drawing.Point(465, 470);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(126, 28);
+            this.btnCancelar.TabIndex = 6;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsslTiempo});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 500);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(623, 22);
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tsslTiempo
+            // 
+            this.tsslTiempo.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.tsslTiempo.Name = "tsslTiempo";
+            this.tsslTiempo.Size = new System.Drawing.Size(96, 17);
+            this.tsslTiempo.Text = "lunes 23/01/2023";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.ClientSize = new System.Drawing.Size(623, 522);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblDireccion);
+            this.Controls.Add(this.lblEdad);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblApellido);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Datos Personales";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblEdad;
+        private System.Windows.Forms.Label lblDireccion;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel tsslTiempo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            //VALIDACION
+            if(txtApellido.Text == "")
+                txtApellido.BackColor = Color.LightCoral;
+            else
+                txtApellido.BackColor = System.Drawing.SystemColors.Window;
+            if (txtNombre.Text == "")
+                txtNombre.BackColor = Color.LightCoral;
+            else
+                txtNombre.BackColor = System.Drawing.SystemColors.Window;
+            if (txtEdad.Text == "")
+                txtEdad.BackColor = Color.LightCoral;
+            else
+                txtEdad.BackColor = System.Drawing.SystemColors.Window;
+            if (txtDireccion.Text == "")
+                txtDireccion.BackColor = Color.LightCoral;
+            else
+                txtDireccion.BackColor = System.Drawing.SystemColors.Window;
+            
+            //RESULTADO DE DATOS
+            string textoApellidoNombre = "Apellido y Nombre: " + txtApellido.Text + " " + txtNombre.Text;
+            string textoEdad = "Edad: " + txtEdad.Text;
+            string textoDireccion = "Dirección: " + txtDireccion.Text;
+            txtResultado.Lines = new string[] { textoApellidoNombre, textoEdad, textoDireccion };
+        }
+        private void txtEdad_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar < 48 || e.KeyChar > 57) && e.KeyChar != 8)
+                e.Handled = true;
+        }
+
+        private void lblApellido_MouseMove(object sender, MouseEventArgs e)
+        {
+            lblApellido.BackColor = Color.Gold;
+            lblApellido.Cursor = Cursors.Hand;
+        }
+
+        private void lblApellido_MouseLeave(object sender, EventArgs e)
+        {
+            lblApellido.BackColor = Color.MediumAquamarine;
+        }
+
+        private void lblNombre_MouseMove(object sender, MouseEventArgs e)
+        {
+            lblNombre.BackColor = Color.Gold;
+            lblNombre.Cursor = Cursors.Hand;
+        }
+
+        private void lblNombre_MouseLeave(object sender, EventArgs e)
+        {
+            lblNombre.BackColor = Color.MediumAquamarine;
+        }
+
+        private void lblEdad_MouseMove(object sender, MouseEventArgs e)
+        {
+            lblEdad.BackColor = Color.Gold;
+            lblEdad.Cursor = Cursors.Hand;
+        }
+
+        private void lblEdad_MouseLeave(object sender, EventArgs e)
+        {
+            lblEdad.BackColor= Color.MediumAquamarine;
+        }
+
+        private void lblDireccion_MouseMove(object sender, MouseEventArgs e)
+        {
+            lblDireccion.BackColor = Color.Gold;
+            lblDireccion.Cursor= Cursors.Hand;
+        }
+
+        private void lblDireccion_MouseLeave(object sender, EventArgs e)
+        {
+            lblDireccion.BackColor= Color.MediumAquamarine;
+        }
+
+        private void lblResultado_MouseMove(object sender, MouseEventArgs e)
+        {
+            lblResultado.BackColor = Color.Gold;
+            lblResultado.Cursor= Cursors.Hand;
+        }
+
+        private void lblResultado_MouseLeave(object sender, EventArgs e)
+        {
+            lblResultado.BackColor = Color.MediumAquamarine;
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
